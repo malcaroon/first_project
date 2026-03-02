7c77080dfe694ce530b2c720b6610eb2ca20a8ae
May-Anne Calundre <mlcalundre3m@gmail.com>
2026-03-02 12:48:11 +0800

    details for create

--- src/index.js
+++ src/index.js
@@ -2,9 +2,26 @@ import express from 'express'
 const app = express()
 const port = 5000
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.post('/user', async (req, res) => {
+  const {first_name, last_name, email, password} = req.body;
+
+  try {
+    res.status(201).json({
+      first_name,
+      last_name,
+      email,
+      password
+    });
+  } catch (error) {
+    res.status(500).json({error: error.message});
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
