6d02c226db5a51d58f1f51e956164a18d345544b
May-Anne Calundre <mlcalundre3m@gmail.com>
2026-03-02 22:43:13 +0800

    READ endpoint

--- src/index.js
+++ src/index.js
@@ -26,6 +26,32 @@ app.post('/register', async (req, res) => {
   }
 });
 
+//READ (all)
+app.get('/items', async (req, res) => {
+  try {
+    res.status(200).json({items: [] });
+  } catch (error) {
+    res.status(500).json({error: error.message});
+  }
+});
+
+//READ (one)
+app.get('/items:id', async (req, res) => {
+  try {
+    const [rows] = await pool.query('SELECT * FROM users WHERE user_id = ?', [
+      req.params.id
+    ])
+
+    if (rows.length === 0) {
+      return res.status(404).json({message: 'User not found!'});
+    }
+
+    res.status(200).json(rows[0]);
+  } catch (error) {
+    res.status(500).json({error: error.message});
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
